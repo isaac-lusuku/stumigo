690acf6a66d2c263c5c9f1cf5427ee976a596e1a
isaac lusuku <isaac256kiwa@gmail.com>
2023-10-17 23:01:43 +0300

    done with the models

--- main/models.py
+++ main/models.py
@@ -36,6 +36,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True, max_length=25, blank=False)
     password = models.CharField(max_length=15, blank=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+    study_mate = models.ManyToManyField("self", blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["password"]
@@ -54,3 +55,69 @@ class UserProfile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+
+
+# topics for discussion
+class Topic(models.Model):
+    name = models.CharField(blank=False, max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+#problems that are put up for discussion
+class Problem(models.Model):
+    asked_by = models.ForeignKey(User, blank=True, on_delete=models.SET_NULL, null=True)
+    question = models.TextField(max_length=100, blank=None)
+    time_asked = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, blank=True, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        ordering = ['-time_asked'] 
+
+    def __str__(self):
+        return self.question[20]
+
+
+# the solutions to the problems pu up
+class Solution(models.Model):
+    answered_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    problem = models.ForeignKey(Problem, blank=False, on_delete=models.CASCADE)
+    answered_at = models.DateTimeField(auto_now_add=True)
+    up_vote = models.IntegerField(default=0)
+    down_vote = models.IntegerField(default=0)
+    answer = models.TextField(blank=False, max_length=150)
+
+    class Meta:
+        ordering = ['-answered_at'] 
+
+    def __str__(self):
+        return self.answer[20]
+
+
+# the discussion rooms for various topics
+class Room(models.Model):
+    name = models.CharField(max_length=25, blank=False)
+    created_by = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, blank=True, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    members = models.ManyToManyField(User, blank=True, related_name="participants")
+    description = models.TextField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+# messages belonging to different rooms
+class Message(models.Model):
+    creator = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, blank=False, on_delete=models.CASCADE)
+    body = models.TextField(max_length=250, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    replied_to = models.ForeignKey("self", blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at'] 
+
+    def __str__(self):
+        return self.body[20]
