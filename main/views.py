e5d540c3a9c07eddb64c5e2d2731a67d4041660e
isaac lusuku <isaac256kiwa@gmail.com>
2023-10-23 23:35:10 +0300

    next we separate the apps

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+
+from .models import *
+from django.db.models import Q
+from .serializers import *
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
+
+
 
 # Create your views here.
