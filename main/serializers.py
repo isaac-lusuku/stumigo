d37241da22523d021cdf441ac709c8d77c9760e0
isaac lusuku <isaac256kiwa@gmail.com>
2023-10-28 23:53:11 +0300

    ...

--- main/serializers.py
+++ main/serializers.py
@@ -22,5 +22,27 @@ class UserProfileSerializer(ModelSerializer):
 
         
 
+from .models import *
+from django.db.models import Q
+from .serializers import *
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
+import re
+
+
+
+# function to validat the email address that was entered 
+def is_valid_email(email):
+    # Define a regular expression for basic email validation
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, email) is not None
+
+
+# this creates and returns the user 
+class UserView(APIView):
+    permission_classes = [AllowAny]
 
         
