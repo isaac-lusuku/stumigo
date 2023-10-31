c853ee72f8b88d8f4c377fb242b2040450b42127
isaac lusuku <isaac256kiwa@gmail.com>
2023-10-31 23:21:17 +0300

    email

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 """
     in this file i write all me  views to serve my API
 """
@@ -10,7 +11,56 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
-import re
+
+
+# import base64
+# from email.message import EmailMessage
+
+# import google.auth
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
+
+
+# def gmail_send_message():
+#     """Create and send an email message
+#     Print the returned  message id
+#     Returns: Message object, including message id
+
+#     Load pre-authorized user credentials from the environment.
+#     TODO(developer) - See https://developers.google.com/identity
+#     for guides on implementing OAuth2 for the application.
+#     """
+#     creds, _ = google.auth.default()
+
+#     try:
+#         service = build('gmail', 'v1', credentials=creds)
+#         message = EmailMessage()
+
+#         message.set_content('This is automated draft mail')
+
+#         message['To'] = 'gduser1@workspacesamples.dev'
+#         message['From'] = 'gduser2@workspacesamples.dev'
+#         message['Subject'] = 'Automated draft'
+
+#         # encoded message
+#         encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+#             .decode()
+
+#         create_message = {
+#             'raw': encoded_message
+#         }
+#         # pylint: disable=E1101
+#         send_message = (service.users().messages().send
+#                         (userId="me", body=create_message).execute())
+#         print(F'Message Id: {send_message["id"]}')
+#     except HttpError as error:
+#         print(F'An error occurred: {error}')
+#         send_message = None
+#     return send_message
+
+
+# if __name__ == '__main__':
+#     gmail_send_message()
 
 
 
