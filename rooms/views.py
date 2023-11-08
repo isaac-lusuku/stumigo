2091623f73704f3ffeb2b968b8e30c48566d3aae
isaac lusuku <isaac256kiwa@gmail.com>
2023-11-08 22:43:18 +0300

    been busy

--- rooms/views.py
+++ rooms/views.py
@@ -7,6 +7,28 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
 import re
+import jwt
+from main.models import *
+
+# the function that returns the authenticated user
+"""
+# this is the command that calls it
+   -->email = auth_user(request.META.get('HTTP_AUTHORIZATION')) <--
+"""
+def auth_user(token):
+    
+        authorization_header = token
+        if authorization_header and authorization_header.startswith('Bearer '):
+            token = authorization_header.split(' ')[1]
+            SECRET_KEY = 'django-insecure--p2@(3we42aui0o1*=4_f%(yn6zcm&@vcuc)lf_s-dvj$m2^1&'
+            try:
+                payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+                return (payload.get("email"))
+            except Exception as e:
+                return False
+        else:
+            return False
+
 
 class TopicView(APIView):
     permission_classes = [IsAuthenticated]
@@ -64,4 +86,61 @@ class MessageView(APIView):
      permission_classes = [IsAuthenticated]
 
     #  creating a message
-    def post
+     def post(self, request):
+         message = MessageSerializer(data=request.data)
+         if message.is_valid():
+             message.save()
+             return Response(status.HTTP_200_OK)
+         else:
+             return Response(status.HTTP_406_NOT_ACCEPTABLE)
+         
+    # editting a message
+     def put(self, request, pk):
+         
+
+         class RoomView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    # creatng a room
+    def post(self, request):
+        room  = RoomSerializer(data=request.data)
+        if room.is_valid():
+            room.save()
+            return Response(status.HTTP_200_OK)
+        else:
+            return Response(status.HTTP_406_NOT_ACCEPTABLE)
+        
+    # getting a room
+    def get(self, request, pk):
+        room = Room.objects.get(id= pk)
+
+        # getting the roo messages
+        messages = room.message_set.all().order_by("-time_created")
+        message_serializer = MessageSerializer(messages, many=True)
+
+        if room:
+            room_serializer = RoomSerializer(room, many=False)
+            message_room = {
+                "room":room_serializer.data,
+                "messages":message_serializer.data,
+            }
+            return Response(message_room, status.HTTP_200_OK)
+        else:
+            return Response(status.HTTP_404_NOT_FOUND)
+        
+
+# a view to deal with individual the messgaes
+class MessageView(APIView):
+     permission_classes = [IsAuthenticated]
+
+    #  creating a message
+     def post(self, request):
+         message = MessageSerializer(data=request.data)
+         if message.is_valid():
+             message.save()
+             return Response(status.HTTP_200_OK)
+         else:
+             return Response(status.HTTP_406_NOT_ACCEPTABLE)
+         
+    # editting a message
+     def put(self, request, pk):
